1d54cc40a7629234c8eaf7f3c21af0ee8bf75208
abdelhak <abdelhak.bouhafs@gmail.com>
2020-12-25 11:27:26 +0100

    login

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,10 +1,27 @@
 import './login.css'
 
-const Login = ()=> {
+const Login = () => {
     return (
-        <h1>Login</h1>
+        <div className={'container'}>
+            <div className={'card'}>
+                <div className={'title'}>
+                    <span className={'title-text'}>Login</span>
+                </div>
+                <div className={'inputs'}>
+                    <input placeholder={'Login'}/>
+                    <input type={'password'} placeholder={'Password'}/>
+                </div>
+                <div className={'actions'}>
+                    <div className={'auth'}>
+                        <span>Se connecter</span>
+                    </div>
+                    <div className={'register'}>
+                        <span>S'inscrire</span>
+                    </div>
+                </div>
+            </div>
 
-
+        </div>
     )
 }
 
