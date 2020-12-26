4d66d051c4ff4a7b5a21cd12ba77dc42c3d612d8
abdelhak <abdelhak.bouhafs@gmail.com>
2020-12-26 11:30:16 +0100

    navbar-v3

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,13 +1,22 @@
 import './navbar.css'
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className={'nav-bar-container'}>
             <div className={'nav-bar-sub-container'}>
-
                 <div className={'nav-bar-left'}>
                 </div>
                 <div className={'nav-bar-right'}>
+                    <div className={'nav-bar-item'}>
+                        <Link to="/">Blog</Link>
+                    </div>
+                    <div className={'nav-bar-item'}>
+                        <Link to="/">Contacter-Nous</Link>
+                    </div>
+                    <div className={'nav-bar-item'}>
+                        <Link to="/login">Login</Link>
+                    </div>
                 </div>
             </div>
         </div>
