73582fb5cea9c99af7f6f8e7cbe281609b06e8cf
abdelhak <abdelhak.bouhafs@gmail.com>
2020-12-26 09:23:39 +0100

    redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Login from "./screens/login/Login";
 import Register from "./screens/register/Register";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import Principal from "./screens/principal/principal";
 
 function App() {
     return (
         <Router>
             <Switch>
+                <Route exact path="/">
+                    <Redirect to="/principal"/>
+                </Route>
+                <Route path="/principal">
+                    <Principal/>
+                </Route>
                 <Route path="/login">
                     <Login/>
                 </Route>
