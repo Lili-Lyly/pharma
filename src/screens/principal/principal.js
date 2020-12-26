d74aac3cc40868223c17c2ae9537d2e9b4fc31b5
Walid Boubechtoula <abdelhak.bouhafs@gmail.com>
2020-12-26 15:25:16 +0100

    add search componenet

--- src/screens/principal/principal.js
+++ src/screens/principal/principal.js
@@ -1,10 +1,12 @@
 import './principal.css'
 import Navbar from "../../components/navbar/navbar";
+import Search from "../../components/search/search";
 
 const Principal = () => {
     return (
         <div className={'principal-all'}>
             <Navbar></Navbar>
+            <Search></Search>
         </div>
     )
 }
