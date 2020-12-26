fceb89d8e9cccb83bc206e270ca4dc5688b06804
Walid Boubechtoula <abdelhak.bouhafs@gmail.com>
2020-12-26 16:26:35 +0100

    add search componenet

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,8 +1,21 @@
 import './search.css'
+import srh from '../../assets/images/noun_Search_860389.svg'
+import logo from '../../assets/images/logo-1502891216.jpg'
 
 const Search = () => {
     return (
-        <h1>Search</h1>
+        <div className={'search-container'}>
+            <div className={'search-part1'}>
+                <img style={{}} className={'search-logo'} src={logo}/>
+            </div>
+            <div className={'search-part2'}>
+                <input placeholder={'Rechercher'} className={'search-input'}/>
+                <div className={'search-icon-container'}>
+                    <img src={srh} className={'search-img'}/>
+                </div>
+            </div>
+            <div className={'search-part3'}></div>
+        </div>
     )
 }
 export default Search;
